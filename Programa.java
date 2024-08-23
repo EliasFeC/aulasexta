ef543f950934cf9352594e0bffa2cbee253f3998
EliasFeC <151461511+EliasFeC@users.noreply.github.com>
2024-08-23 10:15:06 -0300

    Create Programa.java

---
+++ Programa.java
@@ -0,0 +1,8 @@
+public class Programa {
+    public static void main(String[] args) {
+        Universidade universidade = new Universidade();
+        Universidade.setNome("Una");
+
+        System.out.println(Universidade.getNome);
+    }
+}
