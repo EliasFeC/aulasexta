64ac93d6af6dde3644ea6b90a6d3767a07bcce1c
EliasFeC <151461511+EliasFeC@users.noreply.github.com>
2024-08-26 04:19:43 -0300

    Create Atleta.java

---
+++ Atleta.java
@@ -0,0 +1,25 @@
+public class Atleta {
+    private static String Nome;
+    private static String Pais;
+    private static int QtdeMedalhas;
+
+    public static String getPais() {
+        return Pais;
+    }
+    public static void setPais(String pais) {
+        Pais = pais;
+    }
+    public static int getQtdeMedalhas() {
+        return QtdeMedalhas;
+    }
+    public static void setQtdeMedalhas(int qtdeMedalhas) {
+        QtdeMedalhas = qtdeMedalhas;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+}
