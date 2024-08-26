5246c2341b2dfa130a248ee9a4e82a1ac1787593
EliasFeC <151461511+EliasFeC@users.noreply.github.com>
2024-08-26 04:14:44 -0300

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -1,8 +1,12 @@
 public class Programa {
     public static void main(String[] args) {
         Universidade universidade = new Universidade();
-        Universidade.setNome("Una");
+        universidade.setNome("Una");
+        universidade.setEndereço("Aimores");
+        universidade.setCNPJ("xxxxxxxxxx");    
+        universidade.setQtdeSalas(66);    
 
-        System.out.println(Universidade.getNome);
+        System.out.printf("Universidade: " + universidade.getNome() + " Endereço: " + universidade.getEndereço() + " CNPJ: " +  universidade.getCNPJ() + " QtdeSalas: " +  universidade.getQtdeSalas());
     }
+    
 }
