64b3501068d12aedfa587542b954778403630383
EliasFeC <151461511+EliasFeC@users.noreply.github.com>
2024-08-26 04:14:58 -0300

    Update Universidade.java

--- Universidade.java
+++ Universidade.java
@@ -1,35 +1,32 @@
 public class Universidade {
-
     private static String Nome;
-    public static char[] getNome;
+    private static String CNPJ;
+    private static String Endereço;
+    private static int QtdeSalas;
+
     public String getNome() {
         return Nome;
     }
-    public static void setNome(String nome) {
+    public void setNome(String nome) {
         Nome = nome;
     }
-    
-    private int CNPJ;
-    public int getCNPJ() {
+    public String getCNPJ() {
         return CNPJ;
     }
-    public void setCNPJ(int cNPJ) {
+    public void setCNPJ(String cNPJ) {
         CNPJ = cNPJ;
     }
-
-    private String Endereço;
     public String getEndereço() {
         return Endereço;
     }
     public void setEndereço(String endereço) {
         Endereço = endereço;
     }
+    public int getQtdeSalas() {
+        return QtdeSalas;
+    }
+    public void setQtdeSalas(int qtdeSalas) {
+        QtdeSalas = qtdeSalas;
+    }
 
-    private Float QtdSala;
-    public Float getQtdSala() {
-        return QtdSala;
-    }
-    public void setQtdSala(Float qtdSala) {
-        QtdSala = qtdSala;
-    }
 }
